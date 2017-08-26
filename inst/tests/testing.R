08106b87c254c4becdbbf655f712df11f488144a
Your Name Here <lcpaulet@gmail.com>
2017-08-26 22:55:46 +0200

    Commit test 1

--- inst/tests/testing.R
+++ inst/tests/testing.R
@@ -1,7 +1,6 @@
 library(testthat)
-library(BuilidinganRPackageLCP)
 
-test_that("check file name", {
+test_that("testing", {
   filename <- basename(BuilidinganRPackageLCP::make_filename(2013))
   expect_equal(filename, "accident_2013.csv.bz2")
 })
