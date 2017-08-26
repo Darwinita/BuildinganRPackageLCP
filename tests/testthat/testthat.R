b167819b39567a4be3774f6b35e07f504e0607f1
Your Name Here <lcpaulet@gmail.com>
2017-08-26 23:27:13 +0200

    New changes on test

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -4,5 +4,3 @@ test_that("check file name", {
   filename <- basename(BuilidinganRPackageLCP::make_filename(2013))
   expect_equal(filename, "accident_2013.csv.bz2")
 })
-
-test_check("BuilidinganRPackageLCP")
