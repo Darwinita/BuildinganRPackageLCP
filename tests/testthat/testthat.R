0b0e128ae49334efe4fbc9b786cebd47da859d7b
Darwinita <lcpaulet@gmail.com>
2017-08-26 23:10:15 +0200

    Update testthat.R

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -1,14 +1,4 @@
 library(testthat)
-library(BuilidinganRPackageLCP)
-
-filename <- system.file("extdata", "accident_2015.csv.bz2", package = "BuilidinganRPackageLCP")
-dataframe2015 <- BuilidinganRPackageLCP::fars_read(filename)
-class(dataframe2015)
-
-test_that("check data frame", {
-  dataframe2015 <- BuilidinganRPackageLCP::fars_read(filename)
-  expect_that(dataframe2015, is_a("tbl_df"))
-})
 
 test_that("check file name", {
   filename <- basename(BuilidinganRPackageLCP::make_filename(2013))
